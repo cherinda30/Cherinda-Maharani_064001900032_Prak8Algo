b206787b735eb507f3a92cfc9a907f99afea420b
cherinda30 <91549106+cherinda30@users.noreply.github.com>
2021-11-17 15:34:19 +0700

    Add files via upload

---
+++ prak8algo.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Cherinda maharani
+"""
+print("PROGRAM MEMUNCULKAN KARAKTER INDEKS GANJIL")
+masuk = input("Masukkan sebuah kata: ")
+
+print("Karakter indeks ganjil:", masuk[1::2])
